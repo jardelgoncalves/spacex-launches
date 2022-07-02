24a8455888850a4ea6689842218ca8d4e894ce9f
jardelgoncalves <jardelgoncalves1996@gmail.com>
2022-07-01 21:26:06 -0300

    feat: return launches data to page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,31 @@
-import type { NextPage } from 'next'
+import type { GetStaticProps } from 'next'
+import { revalidateWith } from 'utils/revalidate-with'
+import { getLaunchesPast, Launch } from 'graphql/queries/getLaunchesPast'
+import Image from 'next/image'
 
-const Home: NextPage = () => {
+type HomeProps = {
+  launches: Launch[]
+}
+
+const Home = ({ launches }: HomeProps) => {
   return (
-    <h1>SpaceX</h1>
+    <ul>
+      {launches?.map(launch => (
+        <li key={launch?.id}>
+          <Image layout='fill' src={launch.image} alt={launch?.mission_name} />
+        </li>
+      ))}
+    </ul>
   )
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const launches = await getLaunchesPast()
+
+  return {
+    props: { launches },
+    revalidate: revalidateWith('7d')
+  }
 }
 
 export default Home
