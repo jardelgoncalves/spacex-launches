71494e0e9c4594bce247584b1ead40eb185f18cf
jardelgoncalves <jardelgoncalves1996@gmail.com>
2022-07-02 01:07:03 -0300

    refactor: export default page container

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,23 +1,7 @@
 import type { GetStaticProps } from 'next'
 import { revalidateWith } from 'utils/revalidate-with'
-import { getLaunchesPast, Launch } from 'graphql/queries/getLaunchesPast'
-import Image from 'next/image'
+import { getLaunchesPast } from 'graphql/queries/getLaunchesPast'
 
-type HomeProps = {
-  launches: Launch[]
-}
-
-const Home = ({ launches }: HomeProps) => {
-  return (
-    <ul>
-      {launches?.map(launch => (
-        <li key={launch?.id}>
-          <Image layout='fill' src={launch.image} alt={launch?.mission_name} />
-        </li>
-      ))}
-    </ul>
-  )
-}
 
 export const getStaticProps: GetStaticProps = async () => {
   const launches = await getLaunchesPast()
@@ -28,4 +12,4 @@ export const getStaticProps: GetStaticProps = async () => {
   }
 }
 
-export default Home
+export { default } from 'components/pages/Home'
