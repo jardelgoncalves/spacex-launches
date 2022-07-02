7017c0d64ccd7145c546063353685437809f6ea9
jardelgoncalves <jardelgoncalves1996@gmail.com>
2022-07-02 01:00:54 -0300

    feat: add scroll snap

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -9,10 +9,17 @@ export const GlobalStyles = createGlobalStyle`
     margin: 0;
   }
 
+  html {
+    scroll-snap-type: y mandatory;
+    font-size: 16px;
+  }
+
   body {
     ${({ theme }) => css`
       background-color: ${theme.colors.grayscale[950]};
       color: ${theme.colors.grayscale[50]};
+      scroll-snap-type: y mandatory;
+      font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', Droid Sans, 'Helvetica Neue', sans-serif;
     `};
   }
 
