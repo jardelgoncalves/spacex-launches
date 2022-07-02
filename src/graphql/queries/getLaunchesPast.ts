6fe2fe15736974cb9df70b598b832c9024b1441a
jardelgoncalves <jardelgoncalves1996@gmail.com>
2022-07-02 00:59:52 -0300

    refactor: change type name

--- src/graphql/queries/getLaunchesPast.ts
+++ src/graphql/queries/getLaunchesPast.ts
@@ -2,14 +2,14 @@ import { gql } from '@apollo/client'
 import { client } from 'lib/apollo'
 import { launchImageByIndex } from 'utils/launch-image-by-index'
 
-export type Launch = {
+export type LaunchType = {
   id: string
   mission_name: string
   image: string
 }
 
 type LaunchPastResponseType = {
-  launchesPast: Launch[]
+  launchesPast: LaunchType[]
 }
 export const getLaunchesPast = () => {
   const query = gql`
@@ -23,8 +23,8 @@ export const getLaunchesPast = () => {
 
   return client.query<LaunchPastResponseType>({ query })
     .then(({ data: { launchesPast } }) => launchesPast)
-    .then((launchPast) => launchPast.map((launch, index) => ({
+    .then((launchPast) => launchPast.map((launch) => ({
       ...launch,
-      image: launchImageByIndex(index)
+      image: launchImageByIndex(Number(launch.id))
     })))
 }
