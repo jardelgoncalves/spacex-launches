ee7df99e454b7977791b32c8f1947866bdb897d0
jardelgoncalves <jardelgoncalves1996@gmail.com>
2022-07-02 01:01:33 -0300

    feat: add header in app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,11 @@
+import { Header } from 'components/ui/Header'
 import type { AppProps } from 'next/app'
 import { SettingsProvider } from 'providers/SettingsProvider'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <SettingsProvider>
+      <Header />
       <Component {...pageProps} />
     </SettingsProvider>
   )
